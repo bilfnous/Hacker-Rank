51a1661635706282c6b8d03a8fa00fb1480f19d1
B. Alfanous <b.alfanous@outlook.com>
2022-05-03 18:58:26 -0400

    No Prefix Set

---
+++ Week-04/14_No_Prefix_Set/main.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'noPrefix' function below.
+ *
+ * The function accepts STRING_ARRAY words as parameter.
+ */
+
+// Trie Data Structure (Prefix Tree)
+
+const int ALPHABET_SIZE = 26;
+
+struct trieNode {
+    struct trienode *children[ALPHABET_SIZE];
+    bool terminal = false;
+ };
+
+void noPrefix(vector<string> words) {
+
+    // initialize a trie to NULLs
+    struct TrieNode *pNode =  new TrieNode;
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+        pNode->children[i] = NULL;
+
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    vector<string> words(n);
+
+    for (int i = 0; i < n; i++) {
+        string words_item;
+        getline(cin, words_item);
+
+        words[i] = words_item;
+    }
+
+    noPrefix(words);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
